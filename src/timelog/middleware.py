b643111b612d9e3d3e46e5ec1810e5817c2f8ace
ericvoid <eric.void@gmail.com>
2016-03-30 16:46:29 -0300

    fixes time capturing middleware (use time.time instead of time.clock)

--- src/timelog/middleware.py
+++ src/timelog/middleware.py
@@ -9,14 +9,16 @@ logger = logging.getLogger(__name__)
 class TimeLogMiddleware(object):
 
     def process_request(self, request):
-        request._start = time.clock()
+        # py2.7: DO NOT USE time.clock() - although it is used for benchmarking
+        #   time.time() gives more realistic results
+        request._start = time.time()
 
     def process_response(self, request, response):
         # if an exception is occured in a middleware listed
         # before TimeLogMiddleware then request won't have '_start' attribute
         # and the original traceback will be lost (original exception will be
         # replaced with AttributeError)
-        response_time = time.clock() - request._start
+        response_time = time.time() - request._start
 
         sqltime = sum([float(q.get('time', 0.0)) for q in connection.queries])
 
